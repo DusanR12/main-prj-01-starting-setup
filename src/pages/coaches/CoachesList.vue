5aab77c2fd7d8f2b24331de7106b586d47ff8c76
radule01 <dradulovic2001@gmail.com>
2024-03-22 19:54:34 +0100

    working on the Coaches List comp and List Items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,29 +1,32 @@
 <template>
   <section>
-    <h2 style="margin-bottom: 8rem;">Filter</h2>
+    <h2 style="margin-bottom: 8rem">Filter</h2>
   </section>
-  <section>
+  <div>
     <div class="controls">
       <button>Refresh</button>
       <router-link to="/register">Register as a Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in allCoaches" :key="coach.id">
-        <div>
-          <h5>
-            {{ coach.firstName }}
-          </h5>
-        </div>
-      </li>
+      <coach-item v-for="coach in allCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :areas="coach.areas"
+        :hourlyRate="coach.hourlyRate"
+      ></coach-item>
     </ul>
-    <p v-else>
-      No coaches found
-    </p>
-  </section>
+    <p v-else>No coaches found</p>
+  </div>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     allCoaches() {
       return this.$store.getters.coaches;
@@ -31,7 +34,20 @@ export default {
 
     hasCoaches() {
       return this.$store.getters.hasCoaches;
-    }
-  }
-}
+    },
+  },
+};
 </script>
+
+<style scoped>
+  ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
