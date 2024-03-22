8c109fd8e545c3879a2b18113f6b2756f4391a96
radule01 <dradulovic2001@gmail.com>
2024-03-22 18:35:43 +0100

    adding data in the Vuex store

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,6 +7,31 @@
       <button>Refresh</button>
       <router-link to="/register">Register as a Coach</router-link>
     </div>
-    <ul style="font-size: 48px;">Coaches</ul>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in allCoaches" :key="coach.id">
+        <div>
+          <h5>
+            {{ coach.firstName }}
+          </h5>
+        </div>
+      </li>
+    </ul>
+    <p v-else>
+      No coaches found
+    </p>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    allCoaches() {
+      return this.$store.getters.coaches;
+    },
+
+    hasCoaches() {
+      return this.$store.getters.hasCoaches;
+    }
+  }
+}
+</script>
