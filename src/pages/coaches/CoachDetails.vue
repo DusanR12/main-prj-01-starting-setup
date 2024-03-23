b461e94230e78dd617d44f9f919d6baf86450478
radule01 <dradulovic2001@gmail.com>
2024-03-23 16:17:12 +0100

    working on the CoachDetails page

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,5 +1,65 @@
 <template>
-    Details for Coach
-    <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h3>{{ fullName }}</h3>
+      <h4>{{ rate }}</h4>
+    </base-card>
+  </section>
+  <section>
+    <header>
+      <base-card
+        ><h3>Interested? Reach out now!</h3>
+        <base-button link :to="coachContactLink"
+          >Contact</base-button
+        ></base-card
+      >
+    </header>
     <router-view></router-view>
+  </section>
+  <section>
+    <base-card
+      ><base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    viewedCoachId() {
+      return this.$route.params.id;
+    },
+
+    viewedCoach() {
+      const coaches = this.$store.getters.coaches;
+
+      const viewedCoach = coaches.find((coach) => {
+        return coach.id === this.viewedCoachId;
+      });
+
+      return viewedCoach;
+    },
+
+    fullName() {
+      return this.viewedCoach.firstName + ' ' + this.viewedCoach.lastName;
+    },
+
+    rate() {
+      return `$${this.viewedCoach.hourlyRate}/hour`;
+    },
+
+    coachContactLink() {
+      return this.$route.path + '/contact';
+    },
+
+    description() {
+        return this.viewedCoach.description
+    },
+
+    areas() {
+        return this.viewedCoach.areas
+    }
+  },
+};
+</script>
