56f04670dafaf4e875d949d5b23d5ba654f68123
radule01 <dradulovic2001@gmail.com>
2024-03-23 22:16:20 +0100

    fixing the CoachForm component

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,46 +1,46 @@
 <template>
   <section>
     <base-card>
+      <h2>Register as a coach now!</h2>
       <form @submit.prevent="onSubmit">
-        <h3>Register as a coach now!</h3>
-        <div>
+        <div class="form-control">
           <label for="firstname">Firstname</label>
           <input
-            v-model="firstName"
+            v-model.trim="firstName"
             type="text"
             id="firstname"
             name="firstName"
           />
         </div>
-        <div>
+        <div class="form-control">
           <label for="lastname">Lastname</label>
           <input
-            v-model="lastName"
+            v-model.trim="lastName"
             type="text"
             id="lastname"
             name="lastName"
           />
         </div>
-        <div>
+        <div class="form-control">
           <label for="description">Description</label>
           <textarea
-            v-model="description"
+            v-model.trim="description"
             name="description"
             id="description"
             cols="20"
             rows="5"
           ></textarea>
         </div>
-        <div>
+        <div class="form-control">
           <label for="hourlyRate">Hourly Rate</label>
           <input
             v-model="hourlyRate"
-            type="text"
+            type="number"
             id="hourlyRate"
             name="hourlyRate"
           />
         </div>
-        <div>
+        <div class="form-control">
           <h4>Areas of expertise</h4>
           <div>
             <label for="frontend">Frontend Development</label>
@@ -90,12 +90,12 @@ export default {
 
   computed: {
     numOfCoaches() {
-        return this.$store.getters.coaches.length
+      return this.$store.getters.coaches.length;
     },
 
     formattedID() {
-        return 'c' + (this.numOfCoaches + 1);
-    }
+      return 'c' + (this.numOfCoaches + 1);
+    },
   },
 
   methods: {
@@ -107,17 +107,74 @@ export default {
     },
 
     onSubmit() {
-        const newCoach = {
-          id: this.formattedID,
-          firstName: this.firstName,
-          lastName: this.lastName,
-          areas: this.areas,
-          description: this.description,
-          hourlyRate: this.hourlyRate
-        }
+      const newCoach = {
+        id: this.formattedID,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        areas: this.areas,
+        description: this.description,
+        hourlyRate: this.hourlyRate,
+      };
 
-        this.$store.commit('addCoach', newCoach)
+      this.$store.commit('addCoach', newCoach);
     },
   },
 };
 </script>
+
+<style #scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type='checkbox'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type='checkbox']:focus {
+  outline: #3d008d solid 1px;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+</style>
